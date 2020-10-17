fad1561dc5f7be41cca3b811209e967a679cd976
julian salazar <julian.salazard@udea.edu.co>
2020-10-17 16:23:40 -0500

    commit 6, Valores iniciales

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    float *Cdefensivo = NULL, *Cofensivo = NULL, g = 9.8;
+    Cofensivo = new float[5];// canion ofensivo
+    Cdefensivo = new float[5];// canion defensivo
+    int opc = 0;
+    switch (opc) {
+    case 1:{
+
+    };
+        break;
+    case 2:{
+
+    };
+        break;
+    case 3:{
+
+    };
+        break;
+    case 4:{
+
+    }
+        break;
+    default:
+        cout << "Opcion no valida." << endl;
+
+    }
+
     return 0;
 }
